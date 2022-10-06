82c0f7541533906ae72d881693ab793e75e8586c
edwinkay <edwinkaycut@gmail.com>
2022-10-06 16:41:38 -0500

    implementacion login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+  apiKey: "AIzaSyC189vpEmz2Eb0KM9q3qb0zoi1jUohIMKg",
+  authDomain: "loginapppetstore.firebaseapp.com",
+  projectId: "loginapppetstore",
+  storageBucket: "loginapppetstore.appspot.com",
+  messagingSenderId: "93358548667",
+  appId: "1:93358548667:web:260740cd0593220ea42b9e",
+  measurementId: "G-DT65C5NHLW"
+}
 };
